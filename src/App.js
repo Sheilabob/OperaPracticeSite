1122bf93b300a757c73e7a13f1cc7b948c64f5b6
Sheila Strahan <smstrahan@gmail.com>
2022-12-30 10:31:22 -0700

    added words for main theme chorus

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header';
+import MainTheme from './MainTheme';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainTheme />
     </div>
   );
 }
---
+++ src/MainTheme.js
@@ -0,0 +1,18 @@
+function MainTheme() {
+    return (
+      <div>
+        <h2>Main Theme</h2>
+        <p>What can we do to save the world?</p>
+        <p>What can we do to reduce?</p>
+        <p>What can we do to save animals?</p>
+        <p>What can we do to reuse?</p>
+        <p>What can we do to save ourselves?</p>
+        <p>What can we do to recylce?</p>
+        <p>Reduce, Reuse, Recycle</p>
+        <p>Reduce, Reuse, Recycle</p>
+        <p>Reduce, Reuse, Recycle</p>
+      </div>
+    );
+  }
+  
+  export default MainTheme;
