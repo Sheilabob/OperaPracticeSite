d79bb1afc8c4d432993d508f0f03f13e09a4d826
Sheila Strahan <smstrahan@gmail.com>
2022-12-30 18:53:59 -0700

    moved lines into one central act 1 file

---
+++ src/ActOne/ActOne.js
@@ -0,0 +1,18 @@
+import IntroActOne from "./IntroActOne";
+import ActOneLine1 from "./Line1";
+import ActOneLine2 from "./Line2";
+import ActOneLine3 from "./Line3";
+
+function ActOne() {
+
+    return (
+      <div>
+        <IntroActOne />
+        <ActOneLine1 />
+        <ActOneLine2 />
+        <ActOneLine3 />
+      </div>
+    );
+  }
+  
+  export default ActOne;
--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
 import Header from './Header';
 import MainTheme from './MainTheme';
-import IntroActOne from './ActOne/IntroActOne';
 import IntroActTwo from './ActTwo/IntroActTwo';
 import IntroActThree from './ActThree/IntroActThree';
-import ActOneLine1 from './ActOne/Line1';
-import ActOneLine2 from './ActOne/Line2';
-import ActOneLine3 from './ActOne/Line3';
+import ActOne from './ActOne/ActOne';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainTheme />
-      <IntroActOne />
-      <ActOneLine1 />
-      <ActOneLine2 />
-      <ActOneLine3 />
+      <ActOne />
       <IntroActTwo />
       <IntroActThree />
     </div>
